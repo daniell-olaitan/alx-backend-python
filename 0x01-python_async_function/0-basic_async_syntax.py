7add9d55631cb49d126f7cd8930f5331637f8570
daniell-olaitan <daniell.olaitan@gmail.com>
2024-09-01 18:39:29 +0100

    Correct a linter error

--- 0x01-python_async_function/0-basic_async_syntax.py
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -7,6 +7,15 @@ import asyncio
 
 
 async def wait_random(max_delay: int = 10) -> float:
+    """
+    Wait for a given amount of seconds and the returns the time
+
+    Args:
+        max_delay: maximum amount of seconds
+
+    Return:
+        the amount of seconds waited
+    """
     wait_time = random.uniform(0.0, max_delay)
     await asyncio.sleep(wait_time)
 
