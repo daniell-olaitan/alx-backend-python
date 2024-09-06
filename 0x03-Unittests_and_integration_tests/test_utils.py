9aa31ddf9da4c1b61743b591c9337ea64d9a26b1
daniell-olaitan <daniell.olaitan@gmail.com>
2024-09-06 15:47:10 +0100

    Add test cases for utils module

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -3,16 +3,27 @@
 Implement a unittest to test a method
 """
 import unittest
+from unittest.mock import patch, MagicMock
 from parameterized import parameterized
 import typing as t
-from utils import access_nested_map
+import utils
 
 
 class TestAccessNestedMap(unittest.TestCase):
     @parameterized.expand([
-        ('first_test', {"a": 1}, ("a",), 1),
-        ('second_test', {"a": {"b": 2}}, ("a",), {'b': 2}),
-        ('third_test', {"a": {"b": 2}}, ("a", "b"), 2)
+        ('linear map, linear path', {"a": 1}, ("a",), 1),
+        (
+            'nested map (2nd order), linear path',
+            {"a": {"b": 2}},
+            ("a",),
+            {'b': 2}
+        ),
+        (
+            'nested map (2nd order), nested path (2nd order)',
+            {"a": {"b": 2}},
+            ("a", "b"),
+            2
+         )
     ])
     def test_access_nested_map(
         self,
@@ -30,4 +41,77 @@ class TestAccessNestedMap(unittest.TestCase):
             path: sequence of paths
             result: result of the
         """
-        self.assertEqual(access_nested_map(nested_map, path), result)
+        self.assertEqual(utils.access_nested_map(nested_map, path), result)
+
+    @parameterized.expand([
+        ('empty map', {}, ("a",), KeyError, 'a'),
+        ('too long path', {"a": 1}, ("a", "b"), KeyError, 'b')
+    ])
+    def test_access_nested_map_exception(
+        self,
+        description: str,
+        nested_map: t.Mapping,
+        path: t.Sequence,
+        exception: KeyError,
+        exc_message: str
+    ) -> None:
+        """
+        Test the exception of utils.access_nested_map method
+
+        Args:
+            description: description of test
+            nested_map: dictionary of maps
+            path: sequence of paths
+            exception: exception raised
+            exc_message: exception message
+        """
+        with self.assertRaises(exception) as context:
+            utils.access_nested_map(nested_map, path)
+
+        self.assertEqual(context.exception.args[0], exc_message)
+
+
+class TestGetJson(unittest.TestCase):
+    @parameterized.expand([
+        ('http://example.com', {"payload": True}),
+        ('http://holberton.io', {"payload": False})
+    ])
+    @patch('requests.get')
+    def test_get_json(
+        self,
+        test_url: str,
+        test_payload: t.Mapping,
+        mock_response_get: MagicMock
+    ) -> None:
+        """
+        Test a method by mocking an HTTP call
+        """
+        mock_response_get.return_value.json.return_value = test_payload
+        response = utils.get_json(test_url)
+        self.assertEqual(response, test_payload)
+
+
+class TestMemoize(unittest.TestCase):
+    def test_memoize(self):
+        class TestClass:
+            def a_method(self):
+                return 42
+
+            @utils.memoize
+            def a_property(self):
+                return self.a_method()
+
+
+        with patch.object(
+            TestClass,
+            'a_method',
+            return_value = 42
+        ) as mock_method:
+            obj = TestClass()
+            first_result = obj.a_property
+            second_result = obj.a_property
+
+            self.assertEqual(first_result, 42)
+            self.assertEqual(second_result, 42)
+
+            mock_method.assert_called_once()
