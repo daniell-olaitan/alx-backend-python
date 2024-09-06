ea771e6d3afd53859f0d6e6a3138e92bd0f7d701
daniell-olaitan <daniell.olaitan@gmail.com>
2024-09-06 12:02:11 +0100

    Add completed task: 0

---
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+"""
+Implement a unittest to test a method
+"""
+import unittest
+from parameterized import parameterized
+import typing as t
+from utils import access_nested_map
+
+
+class TestAccessNestedMap(unittest.TestCase):
+    @parameterized.expand([
+        ('first_test', {"a": 1}, ("a",), 1),
+        ('second_test', {"a": {"b": 2}}, ("a",), {'b': 2}),
+        ('third_test', {"a": {"b": 2}}, ("a", "b"), 2)
+    ])
+    def test_access_nested_map(
+        self,
+        name: str,
+        nested_map: t.Mapping,
+        path: t.Sequence,
+        result: t.Union[t.Mapping, t.Any]
+    ) -> None:
+        """
+        Test utils.access_nested_map method
+
+        Args:
+            name: name of test
+            nested_map: dictionary of maps
+            path: sequence of paths
+            result: result of the test
+        """
+        self.assertEqual(access_nested_map(nested_map, path), result)
